fc669918d9f63aacd20ea395cdd524199969193e
moshkinv <moshkinvtl@gmail.com>
2017-09-05 18:04:13 +0100

    ONLY STATIC SITE CONCEPTION

--- cms/.settings.js
+++
@@ -1,8 +0,0 @@
-{
-	settings: {
-		$admin_background_image_type: 'image',
-		admin_background_image: 'https://images.pexels.com/photos/94273/jetty-beach-sunset-sea-94273.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
-		dark: true,
-		login_message: ''
-	}
-}
